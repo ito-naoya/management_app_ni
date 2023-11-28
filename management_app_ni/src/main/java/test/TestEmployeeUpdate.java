5fb3bc59aae2194bc77ec831e5a6eeeb0342e7fc
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-11-28 15:51:15 +0900

    更新処理のテストコード追加

--- management_app_ni/src/main/java/test/TestEmployeeUpdate.java
+++ management_app_ni/src/main/java/test/TestEmployeeUpdate.java
@@ -10,4 +10,28 @@ class TestEmployeeUpdate {
 	//		assertEquals(1, updateNum);
 	//	}
 
+	//	@Test
+	//	//パスワードの文字数が８文字未満の場合
+	//	void testPassIsUnder8() throws ClassNotFoundException, NoSuchAlgorithmException, SQLException {
+	//		Employee employee = new Employee(2, "第2部署", "課長", "課長2", "pass");
+	//		String result = EmployeeUpdate.employeeUpdate(employee);
+	//		assertEquals("password is defective", result);
+	//	}
+
+	//	@Test
+	//	//パスワードの文字数が16文字より多い場合
+	//	void testPassIsOver16() throws ClassNotFoundException, NoSuchAlgorithmException, SQLException {
+	//		Employee employee = new Employee(2, "第2部署", "課長", "課長2", "passwordpasswordpassword");
+	//		String result = EmployeeUpdate.employeeUpdate(employee);
+	//		assertEquals("password is defective", result);
+	//	}
+
+	//	@Test
+	//	//所属部署情報が空文字の場合
+	//	void testEmpUpdate() throws ClassNotFoundException, NoSuchAlgorithmException, SQLException {
+	//		Employee employee = new Employee(2, "", "課長", "課長2", "password123");
+	//		String result = EmployeeUpdate.employeeUpdate(employee);
+	//		assertEquals("department is defective", result);
+	//	}
+
 }
