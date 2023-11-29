e73f1f3d88c605bec5610dd557056311b6d1a6f7
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-11-29 15:19:51 +0900

    値の修正

--- management_app_ni/src/main/java/test/TestEmployeeUpdate.java
+++ management_app_ni/src/main/java/test/TestEmployeeUpdate.java
@@ -15,7 +15,7 @@ class TestEmployeeUpdate {
 	@Test
 	//正しい情報が入力された時
 	void testEmpUpdate() throws ClassNotFoundException, NoSuchAlgorithmException, SQLException {
-		Employee employee = new Employee(2, "第1部署", "課長", "課長2", "password");
+		Employee employee = new Employee(2, "第1部署", "課長", "課長2", "password123");
 		String result = EmployeeUpdate.employeeUpdate(employee);
 		assertEquals(1, result);
 	}
